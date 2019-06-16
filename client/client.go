6c87e29ff99891d8df12d28c7543034715dd9934
Mitu217 <mtksk.26884um@gmail.com>
2019-06-17 00:22:06 +0900

    Create Client

---
+++ client/client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	vegeta "github.com/tsenart/vegeta/lib"
+)
+
+func main() {
+	rate := vegeta.Rate{Freq: 100, Per: time.Second}
+	duration := 5 * time.Second
+	targeter := vegeta.NewStaticTargeter(
+		vegeta.Target{
+			Method: "GET",
+			URL:    "http://localhost:3001/",
+		},
+		vegeta.Target{
+			Method: "GET",
+			URL:    "http://localhost:3002/",
+		},
+	)
+	attacker := vegeta.NewAttacker()
+
+	var metrics vegeta.Metrics
+	for res := range attacker.Attack(targeter, rate, duration, "Big Bang!") {
+		metrics.Add(res)
+	}
+	metrics.Close()
+
+	fmt.Printf("99th percentile: %s\n", metrics.Latencies.P99)
+}
